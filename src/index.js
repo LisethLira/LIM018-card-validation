a11171436afb3587d42c4dc0370b7cff265abcb1
LisethLira <liseth.lira.123@gmail.com>
2022-05-30 09:04:25 -0500

    avance de validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+//Obteniendo número de tarjeta del input
+var creditCardNumber = document.getElementById("creditCardNumber").value;
+
+//var warning1= document.getElementById("warning1");
+
+//function warning1(){
+  //  warning1.style.display="block";
+//}
+
+
+var btnValidator = document.getElementById("btnValidator");
+    btnValidator.addEventListener('click',()=>{
+        validator.isValid(creditCardNumber);
+  
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
-const validator = {
-  // ...
-};
+//let creditCardNumber='4083952015263';
 
+const validator = {
+  isValid: function (creditCardNumber){
+  //Pasar a un array y revertir
+    let stringToArray= creditCardNumber.split("").reverse();
+    //console.log(stringToArray);
+    let arrayCardNumber= [];
+    let sumaDigitos=0;
+    let sumaArray=0;
+
+
+    for (let i=0; i<stringToArray.length; i++){
+      arrayCardNumber.push(parseInt(stringToArray[i]));
+    //console.log(arrayCardNumber);
+      if(i%2!=0){
+        arrayCardNumber[i]= arrayCardNumber[i]*2;
+
+      }
+    }
+    //console.log(arrayCardNumber);
+    for (let i=0;i<arrayCardNumber.length; i++){
+    
+      if(arrayCardNumber[i]>9){
+        let newArray=[]; 
+        newArray=arrayCardNumber[i].toString().split("");
+        sumaDigitos=parseInt(newArray[0])+parseInt(newArray[1]);
+        arrayCardNumber[i]=sumaDigitos;
+      }
+    }
+    
+    for(let i=0;i<arrayCardNumber.length;i++){
+      sumaArray= sumaArray+arrayCardNumber[i];
+    }
+    console.log(arrayCardNumber);
+    return sumaArray%10==0;
+
+  }
+}; 
+console.log(validator.isValid('4083952015263'));
 export default validator;
