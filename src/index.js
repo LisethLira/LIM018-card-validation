1bdd8fe27f6f0e54d981ad11e3c0922b6cb97d81
LisethLira <liseth.lira.123@gmail.com>
2022-06-01 11:25:55 -0500

    avance 3. terminando maskify, porfin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
 
 //Obteniendo número de tarjeta del input
-var creditCardNumber = document.getElementById("creditCardNumber").value;
+//let creditCardNumber = document.getElementById("creditCardNumber").value;
 
+//Advertencia de casilla vacia
 //var warning1= document.getElementById("warning1");
 
 //function warning1(){
@@ -12,8 +13,51 @@ var creditCardNumber = document.getElementById("creditCardNumber").value;
 //}
 
 
-var btnValidator = document.getElementById("btnValidator");
+function modify(){
+    let creditCardNumber = document.getElementById("creditCardNumber");
+    let actionModify= validator.maskify(creditCardNumber.value);
+    creditCardNumber.value= actionModify;
+
+}
+
+let btnValidator = document.getElementById("btnValidator");
     btnValidator.addEventListener('click',()=>{
-        validator.isValid(creditCardNumber);
-  
-});
+        //variable número de tarjeta (input)
+        let creditCardNumber = document.getElementById("creditCardNumber").value;
+        //Advertencia de casilla vacia
+        let warning1= document.getElementById("warning1");
+        //let valid= document.getElementById("valid");
+        //let noValid=document.getElementById("noValid");
+        let validation= validator.isValid(creditCardNumber);
+        if (creditCardNumber.trim().length==0){
+            warning1.style.display="block";
+        }
+        else{
+            if(validation==true){
+                //valid.style.display="block";
+                //noValid.style.display="none";
+            }
+            else{
+                //noValid.style.display="block";
+                //valid.style.display="none";
+            }
+
+        }
+
+        modify();
+        
+    });
+
+
+let cardForm = document.getElementById("cardForm");
+
+function reset(){
+    cardForm.reset();
+    let warning1= document.getElementById("warning1");
+    warning1.style.display="none";
+    //valid.style.display="none";
+    //noValid.style.display="none";
+}
+
+let btnReset = document.getElementById("btnReset");
+    btnReset.addEventListener('click', reset);
